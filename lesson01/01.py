87c7564351ade07a772a5706d2d8da57e0105ff5
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-10-04 10:02:12 +0300

    created the new code

--- lesson01/01.py
+++ lesson01/01.py
@@ -1,20 +1,27 @@
 # Напишите программу, которая принимает на вход цифру,
 # обозначающую день недели, и проверяет, является ли этот день выходным.
 
-a = int(input('Введите число: '))
-if a == 1:
-    print('Понедельник')
-elif a == 2:
-    print('Вторник')
-elif a == 3:
-    print('Среда')
-elif a == 4:
-    print('Четверг')
-elif a == 5:
-    print('Пятница')
-elif a == 6:
-    print('Суббота')
-elif a == 3:
-    print('Воскресенье')
+a = input('Введите число: ')
+
+def day_of_week(a):
+    if a == 1:
+        print('Понедельник')
+    elif a == 2:
+        print('Вторник')
+    elif a == 3:
+        print('Среда')
+    elif a == 4:
+        print('Четверг')
+    elif a == 5:
+        print('Пятница')
+    elif a == 6:
+        print('Суббота')
+    elif a == 3:
+        print('Воскресенье')
+    return a
+
+if a.isdigit():
+    day_of_week(int(a))
 else:
     print('Введено неверное значение')
+
